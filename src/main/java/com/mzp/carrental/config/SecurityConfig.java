f27b3d69c4afac033af0f72d0596c4b3331146af
yashbhise8767 <byash0501@gmail.com>
2026-05-05 17:50:47 +0530

    change config file

--- src/main/java/com/mzp/carrental/config/SecurityConfig.java
+++ src/main/java/com/mzp/carrental/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/agency/notifications/**").hasAnyAuthority("Agency")
                         .requestMatchers("/customer/notifications/**").hasAnyAuthority("Customer")
                         .requestMatchers("/admin/**", "/admin/verifications/**").hasAuthority("Admin")
+//                        .requestMatchers("/agency/**").hasAnyAuthority("Agency", "Admin")
                         .requestMatchers("/agency/**").hasAnyAuthority("Agency", "Admin")
                         .requestMatchers("/customer/**, /ratings/cars").hasAnyAuthority("Customer")
                         .requestMatchers("/view/cars/test/**").hasAnyAuthority("User")
